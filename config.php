cfbd43c75d0367d620caa7fbf340da409ed7e0d6
BIPIN SAHANI <bipinsahani786@gmail.com>
2024-07-12 17:50:39 +0530

    front and single page

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php 
+
+$hostname = "http://localhost/news-template/news-template";
+$conn = mysqli_connect("localhost:3307","root","","news-site") or die("connection failed :" . mysqli_connect_error());
+
+?>
