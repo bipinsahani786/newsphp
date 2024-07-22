397a7b9e3dc51cc8e6b6acd6d28c2a4ae2bda3d0
BIPIN SAHANI <bipinsahani786@gmail.com>
2024-07-22 14:30:49 +0530

    searching

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,35 +14,57 @@
     <!-- Custom stlylesheet -->
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-<!-- HEADER -->
-<div id="header">
-    <!-- container -->
-    <div class="container">
-        <!-- row -->
-        <div class="row">
-            <!-- LOGO -->
-            <div class=" col-md-offset-4 col-md-4">
-                <a href="index.php" id="logo"><img src="images/news.jpg"></a>
-            </div>
-            <!-- /LOGO -->
-        </div>
-    </div>
-</div>
-<!-- /HEADER -->
-<!-- Menu Bar -->
-<div id="menu-bar">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <ul class='menu'>
-                    <li><a href='category.php'>Business</a></li>
-                    <li><a href='category.php'>Entertainment</a></li>
-                    <li><a href='category.php'>Sports</a></li>
-                    <li><a href='category.php'>Politics</a></li>
-                </ul>
+    <!-- HEADER -->
+    <div id="header">
+        <!-- container -->
+        <div class="container">
+            <!-- row -->
+            <div class="row">
+                <!-- LOGO -->
+                <div class=" col-md-offset-4 col-md-4">
+                    <a href="index.php" id="logo"><img src="images/news.jpg"></a>
+                </div>
+                <!-- /LOGO -->
             </div>
         </div>
     </div>
-</div>
-<!-- /Menu Bar -->
+    <!-- /HEADER -->
+    <!-- Menu Bar -->
+    <div id="menu-bar">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+
+                    <?php
+                    include "config.php";
+                    if(isset($_GET['cid'])){
+                        $cat_id = $_GET['cid'];
+                    }
+                    // $cat_id = $_GET['cid'];
+                    $sql = "SELECT * FROM category WHERE post > 0";
+                    $result = mysqli_query($conn, $sql) or exit("query failed");
+                    if (mysqli_num_rows($result) > 0) {
+                          $active = "";
+                        ?>
+
+                        <ul class='menu'>
+                        <li><a  href='<?php echo $hostname  ?>'>Home</a></li>
+                            <?php
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                if(isset($_GET['cid'])){
+                                    $active = ($row['category_id'] == $cat_id) ? "active" : "";
+                                }
+                                // $active = ($row['category_id'] == $cat_id) ? "active" : "";
+                                echo "<li><a class='{$active}' href='category.php?cid={$row['category_id']}'>{$row['category_name']}</a></li>";
+                            } ?>
+                        </ul>
+
+                    <?php }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /Menu Bar -->
